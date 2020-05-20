77eea85f45b845a5d1b95ea4e7069f610f2990f2
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-05-21 08:44:18 +1000

    Listen on 127.0.0.1:7878 for connections

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> anyhow::Result<()> {
+    use std::net::TcpListener;
+
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+
+    for stream in listener.incoming() {
+        let stream = stream?;
+
+        println!("Connection established!");
+    }
+
+    Ok(())
 }
